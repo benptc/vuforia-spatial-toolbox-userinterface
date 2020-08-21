76e49a475ac8b27e91a60a8044828ce8e0052a98
Ben Reynolds <breynolds@ptc.com>
2020-08-21 15:23:51 -0400

    created empty module for OCR feature

---
+++ src/gui/ar/ocr.js
@@ -0,0 +1,13 @@
+createNameSpace("realityEditor.gui.ar.ocr");
+
+/**
+ * @fileOverview
+ */
+
+(function(exports) {
+    function initService() {
+        console.log('OCR module initialized');
+    }
+
+    exports.initService = initService;
+})(realityEditor.gui.ar.ocr);
