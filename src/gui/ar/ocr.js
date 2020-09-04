c50d09af68b2d3c9c2747d7ad537347deb463b7b
Ben Reynolds <breynolds@ptc.com>
2020-09-04 13:57:34 -0400

    ocr screenshot is taken every 200ms .... very slow

--- src/gui/ar/ocr.js
+++ src/gui/ar/ocr.js
@@ -5,13 +5,13 @@ createNameSpace("realityEditor.gui.ar.ocr");
  */
 
 (function(exports) {
-    let interval = null;
+    let updateInterval = null;
     
-    let ocrAlgorithm = {
-        processImage: function(imageData) {
-            return 'text';
-        }
-    };
+    // let ocrAlgorithm = {
+    //     processImage: function(_imageData) {
+    //         return 'text';
+    //     }
+    // };
 
     function initService() {
         console.log('OCR module initialized');
@@ -19,11 +19,13 @@ createNameSpace("realityEditor.gui.ar.ocr");
         realityEditor.gui.settings.addToggle('OCR Mode', 'enable text recognition mode', 'ocrMode',  '../../../svg/powerSave.svg', false, function(newValue) {
 
             if (newValue) {
-                interval = setInterval(updateLoop, 300); // use 33 for 30fps
+                updateInterval = setInterval(updateLoop, 500); // use 33 for 30fps
                 document.querySelector('#screenshotHolder').style.display ='inline';
+                
+                // renderScreenshot();
 
             } else {
-                clearInterval(interval);
+                clearInterval(updateInterval);
                 document.querySelector('#screenshotHolder').style.display ='none';
 
             }
@@ -32,15 +34,30 @@ createNameSpace("realityEditor.gui.ar.ocr");
 
     function updateLoop() {
         console.log('ocr update loop');
+        
+        renderScreenshot();
 
-        realityEditor.app.getScreenshot("S", function(base64String) {
-            var screenshotBlobUrl = realityEditor.device.utilities.decodeBase64JpgToBlobUrl(base64String);
-            // to show the screenshot, you would:
-            document.querySelector('#screenshotHolder').src = screenshotBlobUrl;
-            
-            let detectedText = ocrAlgorithm.processImage(screenshotBlobUrl);
-        });
+        // realityEditor.app.getScreenshot("S", function(_base64String) {
+        //     // var screenshotBlobUrl = realityEditor.device.utilities.decodeBase64JpgToBlobUrl(base64String);
+        //     // // to show the screenshot, you would:
+        //     // document.querySelector('#screenshotHolder').src = screenshotBlobUrl;
+        //
+        //     // let detectedText = ocrAlgorithm.processImage(screenshotBlobUrl);
+        // });
     }
 
+    function renderScreenshot() {
+        realityEditor.app.getScreenshot("S", 'realityEditor.gui.ar.ocr.processScreenshot');
+    }
+    
+    function processScreenshot(base64String) {
+        var screenshotBlobUrl = realityEditor.device.utilities.decodeBase64JpgToBlobUrl(base64String);
+        // // to show the screenshot, you would:
+        document.querySelector('#screenshotHolder').src = screenshotBlobUrl;
+
+        // let detectedText = ocrAlgorithm.processImage(screenshotBlobUrl);
+    }
+    
     exports.initService = initService;
+    exports.processScreenshot = processScreenshot;
 })(realityEditor.gui.ar.ocr);
